8cc1a6f140d9d74707d4222c80a24fdcc51a39d3
lcrabbit <lucascoelhodacosta@gmail.com>
2019-12-17 11:32:52 -0300

    Add module

---
+++ README.md
@@ -0,0 +1,55 @@
+# LIM
+
+Lua Input Module library. It has simple asbtractions on top of the love input functions and isn't totally complete yet [1]
+It's a great alternative to make different inputs being handled with only one registered action.
+
+[1] - Input Axis are not handled yet, and gamepad actions must be tested.
+
+## Example
+
+```lua
+local Input = require 'input'
+
+function love.load()
+  input = Input.new()
+  -- To register an action called 'left' which will be triggered with A, left arrow or left mousebutton
+  input:bind(key = { 'a', 'left' }, mouse = left, 'left')
+end
+
+function love.update()
+  if (input:down('left')) then
+    print('left action is down')
+  end
+end
+```
+
+And simple as that your input will be handled automagically.
+
+## API
+
+```lua
+  -- Creates a new input
+  input = Input.new()
+
+  -- Bind an unique key to an action called jump
+  input:bind({ key = {'up'} }, 'jump')
+
+  -- Bind a mouse button and a gamepad button on the same action
+  input:bind({ mouse = 'left', button = 'start' }, 'start')
+
+  -- Input callbacks (called on your update function)
+  -- Check for binded action single press
+  if input:pressed('start') then print('start the game') end
+
+  -- Check for binded action down
+  if input:down('start') then print('holding start action') end
+
+  -- Check for binded action just released
+  if input:down('start') then print('released start action') end
+```
+
+## TODO List
+
+- [] Handle gamepad axis
+- [] Test gamepad functionalities
+- [] Support multiple joysticks (won't resolve until I need it)
---
+++ input.lua
@@ -0,0 +1,122 @@
+local Input = {}
+local mtInput = { __index = Input }
+
+--[[
+  inputs table must be like:
+  { key = 'x',
+    axis = 'y',
+    button: 'dpdown'
+    mouse = 'z'
+  }
+]] --
+
+inputStates = {
+  notpressed = 0,
+  pressed = 1,
+  notclicking = 2,
+  clicking = 3
+}
+
+mouseButtons = {
+  left = 1,
+  right = 2,
+  middle = 3
+}
+
+function Input.new()
+  local input = {}
+  input.binds = {}
+  input.joystick = love.joystick.getJoysticks()[1]
+
+  return setmetatable(input, mtInput)
+end
+
+function Input:bind(inputs, action)
+  -- If we don't have this binding yet, we register it with the desired alias (action name)
+  if (not self.binds[action]) then
+    self.binds[action] = inputs
+    self.binds[action].elapsedTime = 0
+    self.binds[action].delay = 0.3
+    self.binds[action].down = false
+    self.binds[action].prevstate = inputStates.notpressed
+    self.binds[action].state = inputStates.notpressed
+    end
+end
+
+function Input:update()
+  for action, input in pairs(self.binds) do
+    -- We run actions internally for update the state as needed
+    self:released(action)
+    self:pressed(action)
+    self:down(action)
+
+    -- Detects for input changes
+    if (input.key) then
+      for _, key in pairs(input.key) do
+        if (love.keyboard.isDown(key) and self.binds[action].state ~= inputStates.clicking) then
+          self.binds[action].state = inputStates.pressed
+          self.binds[action].down = true
+          break
+        elseif (self.binds[action].state ~= inputStates.clicking) then
+      -- 'Reset' changes if released
+            self.binds[action].state = inputStates.notpressed
+          self.binds[action].down = false
+        end
+      end
+    end
+
+    -- TODO: receive also AXIS and MOUSE inputs
+
+    -- Mouse Handling
+    if (input.mouse) then
+      if (love.mouse.isDown(mouseButtons[input.mouse]) and self.binds[action].state ~= inputStates.pressed) then
+        self.binds[action].state = inputStates.pressed
+        self.binds[action].down = true
+        break
+      elseif (self.binds[action].state ~= inputStates.pressed) then
+        self.binds[action].state = inputStates.notpressed
+        self.binds[action].down = false
+      end
+    end
+
+    -- Joystick Handling: MUST BE TESTED!
+    if (input.button and self.joystick ~= nil) then
+      if (self.joystick:isDown(input.button) and self.binds[action].state ~= inputStates.clicking) then
+        self.binds[action].state = inputStates.pressed
+        self.binds[action].down = true
+      elseif (self.binds[action].state ~= inputStates.clicking) then
+        self.binds[action].state = inputStates.notpressed
+        self.binds[action].down = false
+      end
+    end
+  end
+end
+
+-- Detects single press action
+function Input:pressed(action)
+  if (self.binds[action] ~= nil) then
+    if (self.binds[action].prevstate == inputStates.notpressed and self.binds[action].state == inputStates.pressed) then
+      self.binds[action].prevstate = self.binds[action].state
+      return true
+    end
+  end
+end
+
+-- Detects just release action
+function Input:released(action)
+  if (self.binds[action] ~= nil) then
+    if (self.binds[action].prevstate == inputStates.pressed and self.binds[action].state == inputStates.notpressed) then
+      self.binds[action].prevstate = self.binds[action].state
+      return true
+    end
+  end
+end
+
+-- Detects input holding
+function Input:down(action)
+  if (self.binds[action] ~= nil) then
+    return self.binds[action].down
+  end
+end
+
+return Input
